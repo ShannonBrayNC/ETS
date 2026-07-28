9897814ea314a42327cc884f0c168f68c95f2ba6
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-07-28 00:29:59 -0400

    Add ETS Python testing lab package

---
+++ ets/lab/__init__.py
@@ -0,0 +1,5 @@
+"""Interactive Python testing lab for ETS protocol demonstrations."""
+
+from ets.lab.scenarios import LabRunResult, run_lab_scenario
+
+__all__ = ["LabRunResult", "run_lab_scenario"]
