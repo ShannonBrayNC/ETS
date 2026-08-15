b6e42773bdff5bbd0145e66dcd28dbdcc05a9b61
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-08-14 21:47:34 -0400

    GATE-G2G-Q1: reinstall overlay accessibility manager after sync

--- apps/console/web/src/main.tsx
+++ apps/console/web/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ProductionApp } from "./ProductionApp";
+import { installOverlayAccessibility } from "./overlayAccessibility";
 import "./styles.css";
 import "./dark-pro.css";
+
+installOverlayAccessibility();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
